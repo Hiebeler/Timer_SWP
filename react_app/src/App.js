70b82dcb9727f9df31f78aa15be7fc6b76c52102
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2024-02-16 16:53:43 +0100

    changed style in react

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -35,7 +35,7 @@ function App() {
         <input disabled={started} placeholder='seconds' type='number' value={inputTime} onChange={(e) => {setInputTime(e.target.value); setTime(e.target.value)}} class="w-44 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         <button onClick={start} disabled={started} className='text-xl font-bold text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Start</button>
       </div>
-      <div className='text-white text-center'>
+      <div className='text-white text-center mt-10 text-5xl'>
         {Math.round(time)}
       </div>
     </div>
